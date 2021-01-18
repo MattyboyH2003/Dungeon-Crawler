e11bb29f37a192b2d1d9cad74e9479f418aaee08
Mattyboy <Matthewhunter773@btinternet.com>
2021-01-18 22:43:18 +0000

    Added to main.py and renamed some sprites
    main.py:
    Added TextBox Class for text objects
    Started plotting out the layout for the main menu

--- py/main.py
+++ py/main.py
@@ -58,35 +58,86 @@ class Button(pygame.sprite.Sprite):
     def OnClick(self):
         self.func(**self.params)
 
+class TextBox: #TextBox class for creating text boxes
+    def __init__(self, text, pos, font="comicsansms", fontSize=20, fontColour=colours["black"], tags=[]):
+        self.text = text
+        self.pos = pos
+        self.fontSize = fontSize
+        self.fontColour = fontColour
+        self.font = font
+        self.tags = tags
+        
+    def Draw(self): #called each frame, draws the object to the screen
+        textSurf = pygame.font.SysFont(self.font, self.fontSize).render(self.text, True, self.fontColour)
+        textRect = textSurf.get_rect()
+        textRect.center = (self.pos) 
+        window.blit(textSurf, textRect) 
+
+    def GetTags(self):
+        return self.tags
+
+    def SetText(self, newText):
+        self.text = newText
+
+    def SetPos(self, newPos):
+        self.pos = newPos
+
+    def GetText(self):
+        return self.text
+
 ########################################################################################################
 #                                            - Functions -                                             #
 ########################################################################################################
 
 def MainMenu():
+    #Set Starting Variables
     mainMenu = True
+    customObjectsList = []
     allSpritesList = pygame.sprite.Group()
     buttonList = pygame.sprite.Group()
     
+    #Add the play button to the menu
     playButton = Button((int(resolution[0] / 2), int(resolution[1] / 2)), "Sprites/MainMenuSprites/PlayDefault.png", "Sprites/MainMenuSprites/PlayHover.png", ChooseSave, tags=["Button", "MainMenu"])
     allSpritesList.add(playButton)
     buttonList.add(playButton)
     
-    while mainMenu:
-        window.fill((255, 255, 255))
-        mouse = pygame.mouse.get_pos()
+    #Add the title text
+    title = TextBox("Dungeon Crawler", (400, 135), fontSize=100, tags=["TextBox", "MainMenu"])
+    customObjectsList.append(title)
 
-        #Check events 'n' shit
+    #While loop for the running of the menu
+    while mainMenu:
+        window.fill((255, 255, 255)) #Clear the screen
+        mouse = pygame.mouse.get_pos() #Get the position of the mouse this frame
+
+        #Check each event this frame
         for event in pygame.event.get():
+            #If the user is exiting the application
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             
+            #Upon mouseclick
             if event.type == pygame.MOUSEBUTTONDOWN:
                 clicked = [s for s in buttonList if s.rect.collidepoint(mouse)]
                 for button in clicked:
                     button.OnClick()
         
-        #Set Button Visuals
+        #
+        # Temporary lines for plotting main menu
+        #
+
+        pygame.draw.line(window, colours["black"], (0, 570), (1280, 570), 1)
+        pygame.draw.line(window, colours["black"], (0, 470), (1280, 470), 1)
+        pygame.draw.line(window, colours["black"], (0, 370), (1280, 370), 1)
+        pygame.draw.line(window, colours["black"], (0, 270), (1280, 270), 1)
+
+        #
+        #
+        #
+
+
+        #Draw Button Visuals
         hoveredButtons = [s for s in buttonList if s.rect.collidepoint(mouse)]
         notHoveredButtons = [s for s in buttonList if not s.rect.collidepoint(mouse)]
 
@@ -95,6 +146,11 @@ def MainMenu():
         for button in hoveredButtons:
             button.OnHover()
 
+        #Draw all custom objects
+        for item in customObjectsList:
+            item.Draw()
+        
+        #Final things
         allSpritesList.draw(window)
         pygame.display.update()
         clock.tick(30)
